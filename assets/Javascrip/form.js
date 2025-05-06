3f9dd45b964f653748d23ca44df72d5efcca0661
farisrahman674 <farisrahman674@gmail.com>
2025-05-06 13:43:22 +0700

    Menambahkan js

--- assets/Javascrip/form.js
+++ assets/Javascrip/form.js
@@ -2,6 +2,16 @@ function form_js(event) {
   event.preventDefault();
 
   const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
 
-  console.log(name);
+  console.log(name, email, phone, subject, message);
+
+  Swal.fire({
+    title: "Good job!",
+    text: "You clicked the button!",
+    icon: "success",
+  });
 }
